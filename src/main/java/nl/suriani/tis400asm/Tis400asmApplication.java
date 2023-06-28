bf8fbb0d265a27e1f54029cb3335d33042bd4d3c
suriani <tristano.suriani@ordina.nl>
2023-06-28 09:22:29 +0200

    initial commit

---
+++ src/main/java/nl/suriani/tis400asm/Tis400asmApplication.java
@@ -0,0 +1,13 @@
+package nl.suriani.tis400asm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tis400asmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tis400asmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/suriani/tis400asm/Tis400asmApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.suriani.tis400asm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tis400asmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
